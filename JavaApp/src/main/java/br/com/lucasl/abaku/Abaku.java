a58d78f13e8c97933f752ea37f12af86236c5300
lucasl36 <lucaslopes_@outlook.com>
2022-09-24 21:35:01 -0300

    v0.1.1 - Starting java application project;

---
+++ JavaApp/src/main/java/br/com/lucasl/abaku/Abaku.java
@@ -0,0 +1,12 @@
+package br.com.lucasl.abaku;
+
+/**
+ *
+ * @author Lucas Lopes
+ */
+public class Abaku {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
